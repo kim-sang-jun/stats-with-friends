69dfe5a60bca848c843b06c3c02379dd99159b0f
Bryan Smith <bry-guy@github.com>
2021-11-17 20:35:35 -0800

    Swagger points to prod URL.

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -27,7 +27,7 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: 'www.statswithfriends.com'
             }
           }
         }
